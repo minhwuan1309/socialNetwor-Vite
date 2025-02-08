0a51ab4911d2ea37977afe22e6dd9dbbbcb07058
spidey1309 <164634272+spidey1309@users.noreply.github.com>
2025-02-09 00:35:51 +0700

    Normalize line endings

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,19 @@
 const isAuth = require("../middlewares/isAuth")
 const ctrls = require("../controllers/user")
+const uploadFile = require("../middlewares/multer")
 
 const router = require("express").Router()
 
+//GET
 router.get('/current', isAuth, ctrls.myProfile)
-router.get('/:id', ctrls.userProfile)
+router.get('/:id', isAuth, ctrls.userProfile)
+router.get('/followdata/:id', isAuth, ctrls.userFollowerAndFollowingData)
+
+//PUT
+router.put('/:id', isAuth, uploadFile, ctrls.updateProfile)
+router.put('/password/:id', isAuth, ctrls.updatePassword)
+
+//POST
+router.post('/follow/:id', isAuth, ctrls.followAndUnfollow)
 
 module.exports = router
